c791f251965393ae56e2b6145fe0a6bec13b2ee3
Kiran <kiranbose@gmail.com>
2016-01-15 13:34:53 +0530

    added gulp to create build folder

---
+++ app/js/app.js
@@ -0,0 +1,63 @@
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
+
+function myFunction() {
+    document.getElementById("demo").innerHTML = "Paragraph changed!";
+    document.getElementById("testtext").innerHTML = "Paragraph changed!";
+}
+
+$( document ).ready(function() {
+    document.getElementById("testtext").innerHTML = "lorem ipsum!";
+});
